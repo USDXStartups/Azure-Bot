f94fa532087289ac5cde46e513a50c2dcbd735e3
Lauren Tran <latra@microsoft.com>
2016-12-05 14:35:24 -0800

    first commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var recognizer = new builder.LuisRecognizer(model);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog('/', intents);
 
-// Add intent handlers
+
 intents.matches('CreateVM', [
     function (session, args, next) {
         // Resolve and store any entities passed from LUIS.
@@ -33,7 +33,6 @@ intents.matches('CreateVM', [
         var vm = session.dialogData.vm = {
           vmType: vmType ? vmType.entity : null
         };
-        
         // Prompt for vmType
         if (!vm.vmType) {
             builder.Prompts.text(session, 'Windows or Linux?');
@@ -43,14 +42,57 @@ intents.matches('CreateVM', [
     },
     function (session, results, next) {
         var vm = session.dialogData.vm;
-        if (results.response) {
+        if (results.response.toLowerCase() === 'windows' || results.response.toLowerCase() === 'linux') {
             vm.vmType = results.response;
+            next();
+        } else {
+            session.endDialog("I'm sorry, I don't know about %s virtual machines", results.response);
         }
-        next();
+        //next();
     },
     function (session, results) {
         var baseUrl = 'https://azure.microsoft.com/en-us/documentation/services/virtual-machines/'
         session.send("Here's how to get started with %s virtual machines: " + baseUrl + "%s/", session.dialogData.vm.vmType, session.dialogData.vm.vmType);
+    }
+]);
+
+intents.matches('GetRegions', [
+    function (session, args, next) {
+        //TODO: Add location-specific logic.
+        session.send("Azure currently has datacenters in the following locations:\nVirginia, Iowa, Illinois, Texas, California, Quebec City, Toronto, Sao Paulo State, Ireland, Netherlands, Frankfurt, Magdeburg, Cardiff, Singapore, Hong Kong, New South Wales, Victoria, Pune, Mumbai, Chennai, Tokyo, Osaka, Shanghai, Beijing, Seoul. For more info, see [Azure Regions](https://azure.microsoft.com/en-us/regions/)");
+    }
+]);
+
+intents.matches('GetPricingInfo', [
+    function (session, args, next) {
+        //TODO: Add service-specific logic.
+        session.send("To get a pricing estimate for your specific scenario, check out the Azure pricing calculator: https://azure.microsoft.com/en-us/pricing/calculator/");
+    }
+]);
+
+intents.matches('GetStarted', [
+    function (session, args, next) {
+        //TODO: Add service-specific logic.
+        session.send("Here are some resources to get you started: [Azure Documentation](https://docs.microsoft.com/en-us/azure/), [Azure for Startups GitHub Repository](https://github.com/Azure-for-Startups/Content/blob/master/README.md), [Get Started Guide for Azure Developers](https://opbuildstorageprod.blob.core.windows.net/output-pdf-files/en-us/guides/azure-developer-guide.pdf), [Azure Tools and SDKs](https://docs.microsoft.com/en-us/azure/#pivot=sdkstools)");
+    }
+]);
+
+intents.matches('GetManagementInfo', [
+    function (session, args, next) {
+        session.send("You can create and manage your Azure services programmatically or through the [Azure Portal](portal.azure.com). If you're a Mac user, install the [Azure CLI](https://docs.microsoft.com/en-us/azure/xplat-cli-install), and for Windows, leverage [Azure Powershell commandlets](https://docs.microsoft.com/en-us/powershell/azureps-cmdlets-docs/).  Or if you want, call the REST APIs directly: [Azure REST SDK reference](https://docs.microsoft.com/en-us/rest/api/).  And finally, [Azure Resource Manager](https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-overview)...use this when you want a template-based deployment for all the things.  There's a bunch of [Quickstart templates](https://github.com/Azure/azure-quickstart-templates) already on GitHub that you can start with.");
+    }
+]);
+
+intents.matches('GetAWSTranslation', [
+    function (session, args, next) {
+        //TODO: Add service-specific logic
+        session.send("Check out this [Azure and AWS](https://azure.microsoft.com/en-us/overview/azure-vs-aws/mapping/) chart where you can see what services map to what.");
+    }
+]);
+
+intents.matches('None', [
+    function (session, args, next) {
+        session.send("I'm sorry I didn't understand.");
     }
 ]);
 
